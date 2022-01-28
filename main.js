63c4f700b9f675295efad9d916058a65926255a9
Jim <jim@advancebass.com>
2022-01-27 17:11:59 -0800

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+const keysPressed = [];
+let keyCode = '';
+
+function render(){
+  document.querySelector('#app').innerHTML = `
+    <h1 class="heading-1">Whats The Key Code</h1>
+    <p class="key-code">${keyCode || 'keyCode'}</p>
+    <h2 class="heading-2">Keys Pressed</h2>
+    <p class="keys-pressed">${JSON.stringify(keysPressed)}</p>
+  `;
+}
+
+document.addEventListener('keydown', (e) => {
+  keyCode=e.code;
+  keysPressed.push(keyCode);
+  render();
+});
+
+
+render();
