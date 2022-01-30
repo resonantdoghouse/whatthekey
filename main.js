9756bc62d69e66c64e903c829bd1bab6b9403bde
Jim <jim@advancebass.com>
2022-01-30 16:14:55 +0000

    testing nova editor

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-import './style.css';
+import "./style.css";
 
 const keysPressed = [];
-let keyCode = '';
+let keyCode = "";
 
-function render(){
-  document.querySelector('#app').innerHTML = `
+function render() {
+  document.querySelector("#app").innerHTML = `
     <h1 class="heading-1">Whats The Key Code</h1>
-    <p class="key-code">${keyCode || 'keyCode'}</p>
+    <p class="key-code">${keyCode || "keyCode"}</p>
     <h2 class="heading-2">Keys Pressed</h2>
     <p class="keys-pressed">${JSON.stringify(keysPressed)}</p>
   `;
 }
 
-document.addEventListener('keydown', (e) => {
-  keyCode=e.code;
+document.addEventListener("keydown", (e) => {
+  keyCode = e.code;
   keysPressed.push(keyCode);
   render();
 });
-
 
 render();
